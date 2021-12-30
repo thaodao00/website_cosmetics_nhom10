ad03d6ea9b0916af54c0b7dda08affd4d5c9e2ba
yangheenlw <19130093@st.hcmuaf.edu.vn>
2021-12-31 00:17:57 +0700

    database & user bean

---
+++ database/role.sql
@@ -0,0 +1,2 @@
+INSERT INTO `role` (`id`, `name`, `code`) VALUES (0, 'user', 'user');
+INSERT INTO `role` (`id`, `name`, `code`) VALUES (1, 'admin', 'admin');
--- src/main/java/com/example/website_cosmetics_nhom10/beans/User.java
+++ src/main/java/com/example/website_cosmetics_nhom10/beans/User.java
@@ -4,5 +4,98 @@ public class User {
     private long id, phone, roleId, cartId;
     private String username, password, email, country, avatar;
 
+    public User() {
 
+    }
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public User(long id, long phone, long roleId, long cartId, String username, String password, String email, String country, String avatar) {
+        this.id = id;
+        this.phone = phone;
+        this.roleId = roleId;
+        this.cartId = cartId;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.country = country;
+        this.avatar = avatar;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getPhone() {
+        return phone;
+    }
+
+    public void setPhone(long phone) {
+        this.phone = phone;
+    }
+
+    public long getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(long roleId) {
+        this.roleId = roleId;
+    }
+
+    public long getCartId() {
+        return cartId;
+    }
+
+    public void setCartId(long cartId) {
+        this.cartId = cartId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    
 }
